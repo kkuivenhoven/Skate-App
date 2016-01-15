394382b43c691e808c8799833ecf387eff1b4ab3
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2016-01-15 14:53:48 -0800

    Added tests to ratings model for rating model validations

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -11,4 +11,54 @@ class RatingTest < ActiveSupport::TestCase
     assert @rating.valid?
   end
  
+  test "description should be present" do
+    @rating.description = "   "
+    assert_not @rating.valid?
+  end
+  
+  test "description should be at least 4 characters long" do
+    @rating.description = "a"*2
+    assert_not @rating.valid?
+  end
+ 
+  test "difficulty should be integer" do
+    @rating.difficulty = "lkj" 
+    assert_not @rating.valid?
+  end
+
+  test "difficulty should be greater than or equal to 0" do
+    @rating.difficulty = -1
+    assert_not @rating.valid?
+  end
+
+  test "police should be less than or equal to 10" do
+    @rating.police = 11
+    assert_not @rating.valid?
+  end
+
+  test "police should be greater than or equal to 0" do
+    @rating.police = -1
+    assert_not @rating.valid?
+  end
+
+  test "police should be integer" do
+    @rating.police = "lkj" 
+    assert_not @rating.valid?
+  end
+
+  test "pedestrian should be less than or equal to 10" do
+    @rating.pedestrian = 11
+    assert_not @rating.valid?
+  end
+
+  test "pedestrian should be greater than or equal to 0" do
+    @rating.pedestrian = -1
+    assert_not @rating.valid?
+  end
+
+  test "pedestrian should be integer" do
+    @rating.pedestrian = "lkj" 
+    assert_not @rating.valid?
+  end
+
 end
