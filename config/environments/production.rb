9cf8ebe3b8a5eabb5bd3608ace3b0ad63f5cedaa
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2017-11-20 10:24:51 -0800

    does this work for JS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
   # config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  # config.assets.compile = true
+  # config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
