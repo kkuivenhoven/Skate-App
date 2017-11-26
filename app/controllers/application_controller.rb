f85a2be49b34ba0dace0e17678cc705bb1b6b37c
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2017-11-26 09:07:21 -0800

    added method for detecting mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+	before_action :on_mobile?
+
+	def on_mobile?
+		if session[:mobile_param]
+			session[:mobile_param] == "1"
+		else
+			# request.user_agent =~ /Mobile|webOS/
+			request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
+		end
+		flash[:info] = "request.user_agent is: #{request.user_agent}"
+	end
 
   private
    
