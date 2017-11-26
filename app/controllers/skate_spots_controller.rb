ce7889fb33fc59d395030769292e1683171cbe36
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2017-11-26 11:47:34 -0800

    Fixed recommender error for when third tier of recommendations is 0

--- app/controllers/skate_spots_controller.rb
+++ app/controllers/skate_spots_controller.rb
@@ -334,114 +334,25 @@ class SkateSpotsController < ApplicationController
 			  @m_ids.delete_if {|s| s == @skate_spot.id}
 				@m_skateSpots = SkateSpot.find(@m_ids)
 			end 
-
-
-=begin
-			@skate_spots.each do |s|
-					if s.ratings.count != 0
-						if s.id != @skate_spot.id
-								@stuff = s.ratings.where(:difficulty => @lowdiff..@highdiff, :police => @lowSec..@highSec)
-								# @filter_diff = s.ratings.where(:difficulty => @lowdiff..@highdiff)
-								# if @filter_diff.count != 0
-							# 			@filter_sec = @filter_diff.where(:police => @lowSec..@highSec)
-							# 			if @filter_sec.count != 0
-							# 			    @filter_ped = @filter_sec.where(:pedestrian => @lowPed..@highPed)
-							# 			    if @filter_ped.count != 0
-							# 								@rate_skate << s.ratings
-					     #          end
-					      #     end
-					      # end
-					  end
-					end
-			end
-		end
-		@ss_ids = Array.new
-		if @rate_skate.count > 0
-				# @rate_skate.delete_if { |difficulty| difficulty != @diffAvg }
-				@rate_skate.each do |r_s|
-						@ss_ids << r_s[0].skate_spot_id
-				    # r_s.delete_if { |difficulty| difficulty != @diffAvg }
-        end
-    end
-		if @ss_ids.count > 0 
-			@final_spots = SkateSpot.where(id: @ss_ids)
-		end
-=end
-=begin
-		# if @final_spots.count > 0 
-		if !@final_spots.nil?
-			 @final_spots.each do |f_s|
-					# removes SS if category of f_s doesn't match @skate_spot category
-				  if f_s.street_spot != @skate_spot.street_spot	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.park_spot != @skate_spot.park_spot	
-							@final_spots.delete(f_s)
-			    end
-
-					# removes SS if gated of f_s doesn't match @skate_spot gated
-				  if f_s.gated != @skate_spot.gated	
-						  # @g_to_delete = f_s
-							# @final_spots.reject!{|ss| ss == @g_to_delete}
-							# @index = @final_spots.index(f_s)
-							@final_spots.delete(f_s)
-			    end
-
-					# removes SS if material of f_s doesn't match @skate_spot material
-				  if f_s.wood != @skate_spot.wood	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.metal != @skate_spot.metal	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.concrete != @skate_spot.concrete	
-							@final_spots.delete(f_s)
-			    end
-
-					# removes SS if type of f_s doesn't match @skate_spot type
-				  if f_s.transition != @skate_spot.transition	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.street_plaza != @skate_spot.street_plaza	
-							@final_spots.delete(f_s)
-			    end
-
-					# removes SS if spot size of f_s doesn't match @skate_spot spot size
-				  if f_s.neighborhood_spot_size != @skate_spot.neighborhood_spot_size	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.regional_spot_size != @skate_spot.regional_spot_size	
-							@final_spots.delete(f_s)
-			    end
-				  if f_s.skate_spot_size != @skate_spot.skate_spot_size	
-							@final_spots.delete(f_s)
-			    end
-
-			 end
-=end
     end
   end
+
 
   def new_by_geo
     @skate_spot = SkateSpot.new
   end
+
 
   def create_by_geo
     @skate_spot = SkateSpot.new(geo_skate_spot_params)
 		geo_localization = "#{@skate_spot.latitude},#{@skate_spot.longitude}"
 		@query = Geocoder.search(geo_localization).first
 		@addr_comp = @query.data["address_components"]
-		# @skate_spot.number = @addr_comp.first["long_name"]
 		@skate_spot.number = @addr_comp[0]["long_name"]
-		# @skate_spot.street = @addr_comp.second["long_name"]
 		@skate_spot.street = @addr_comp[1]["long_name"]
-		# @skate_spot.city = @addr_comp.fourth["long_name"]
 		@skate_spot.city = @addr_comp[2]["long_name"]
-		# @skate_spot.state = @addr_comp[5]["short_name"]
 		@skate_spot.state = @addr_comp[4]["short_name"]
-		# @skate_spot.zip_code = @addr_comp[7]["long_name"]
 		@skate_spot.zip_code = @addr_comp[6]["long_name"]
-		# @skate_spot.country = @addr_comp[6]["short_name"]
 		@skate_spot.country = @addr_comp[5]["short_name"]
 		@skate_spot.user_id = current_user.id
     if @skate_spot.save
@@ -471,9 +382,7 @@ class SkateSpotsController < ApplicationController
 
   def update
     @skate_spot = SkateSpot.find(params[:id])
-    #if @skate_spot.update(params.require(:skatespot).permit(:name, :zip_code))
     if @skate_spot.update(skate_spot_params)
-      #redirect_to @skate_spot, notice: "Skatespot has been successfully updated!"
 			flash[:success] = "Skatespot has been successfully updated!"
       redirect_to @skate_spot
     else
@@ -503,20 +412,15 @@ class SkateSpotsController < ApplicationController
 	end
 
   private
+
     #Use callbacks to share common setup or constraints between actions
-#    def set_skate_spot
-#      @skate_spot = SkateSpot.find(params[:id])
-#    end
 
     def geo_skate_spot_params
-      # params.require(:skate_spot).permit(:name, :latitude, :longitude, :park_spot, :street_spot)
-      # params.require(:skate_spot).permit(:name, :latitude, :longitude, :park_spot, :street_spot, :gated, :wood, :metal, :transition, :street_plaza, :skate_spot_size, :neighborhood_spot_size, :regional_spot_size)
       params.require(:skate_spot).permit(:name, :latitude, :longitude, :park_spot, :street_spot, :gated, :wood, :metal, :transition, :street_plaza, :skate_spot_size, :neighborhood_spot_size, :regional_spot_size, :wcmx_accessible)
     end
 
     def skate_spot_params
       params.require(:skate_spot).permit(:name, :number, :street, :city, :state, :country, :zip_code, :park_spot, :street_spot, :gated, :wood, :metal, :transition, :street_plaza, :skate_spot_size, :neighborhood_spot_size, :regional_spot_size, :wcmx_accessible)
-      # params.require(:skate_spot).permit(:name, :number, :street, :city, :state, :country, :zip_code, :park_spot, :street_spot, :gated, :wood, :metal, :transition, :street_plaza, :skate_spot_size, :neighborhood_spot_size, :regional_spot_size)
     end
 
 end
