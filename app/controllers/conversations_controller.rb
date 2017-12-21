869de2609a052cf59aefca2a237fe9db3185ff6e
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2017-12-21 12:33:59 -0800

    changed tutorial methods for conversation to methods I understand better

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,9 +3,33 @@ class ConversationsController < ApplicationController
 	layout false
 
 	def create 
+=begin
 		if Conversation.between(params[:sender_id],params[:recipient_id]).present?
 			@conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
 		else
+			@conversation = Conversation.create!(conversation_params)
+		end
+=end
+		convExists = 0
+    if Conversation.where(sender_id: params[:sender_id], recipient_id: params[:recipient_id]).any?
+			convExists = 1
+    # if Conversation.where("(#{@conversations.sender_id} = ? AND #{@conversations.recipient_id} = ?) OR (#{@conversations.sender_id} ? AND #{@conversations.recipient_id} = ?)", params[:sender_id], params[:recipient_id], params[:recipient_id], params[:sender_id]).any?
+    #works if Conversation.where("(sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)", params[:sender_id], params[:recipient_id], params[:recipient_id], params[:sender_id]).any?
+			# sender_id: params[:sender_id], recipient_id: params[:recipient_id]).any?
+      # @conversation = Conversation.where(sender_id: params[:sender_id], recipient_id: params[:recipient_id]).first.id
+      # @conversation = Conversation.where(sender_id: params[:sender_id], recipient_id: params[:recipient_id]).first
+			# @conversation = Conversation.where("(@conversations.sender_id = ? AND @conversations.recipient_id = ?) OR (@conversations.sender_id ? AND @conversations.recipient_id = ?)", params[:sender_id], params[:recipient_id], params[:recipient_id], params[:sender_id]).first
+			#works @conversation = Conversation.where("(sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)", params[:sender_id], params[:recipient_id], params[:recipient_id], params[:sender_id]).first
+    # elsif Conversation.where(recipient_id: params[:recipient_id], sender_id: params[:sender_id]).any?
+    elsif Conversation.where(sender_id: params[:recipient_id], recipient_id: params[:sender_id]).any?
+			convExists = 1
+      # @conversation = Conversation.where(recipient_id: params[:recipient_id], sender_id: params[:sender_id]).first.id
+      # @conversation = Conversation.where(recipient_id: params[:recipient_id], sender_id: params[:sender_id]).first
+      # @conversation = Conversation.where(sender_id: params[:recipient_id], recipient_id: params[:sender_id]).first
+		end
+    if convExists == 1
+			@conversation = Conversation.where("(sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)", params[:sender_id], params[:recipient_id], params[:recipient_id], params[:sender_id]).first
+    else
 			@conversation = Conversation.create!(conversation_params)
 		end
 
@@ -14,7 +38,18 @@ class ConversationsController < ApplicationController
 
 	def show
 		@conversation = Conversation.find(params[:id])
-		@reciever = interlocutor(@conversation)
+		# @reciever = interlocutor(@conversation)
+		if current_user == @conversation.recipient
+			# means if cur_user started conversation/hit send message button
+			# but cur_user is not stored as sender but stored as recipient
+			# need to indicate that whomever is stored as sender is now the reciever
+			@reciever = @conversation.sender
+		else
+			# indicates that cur_user started conversation/hit send message button
+			# and cur_user is stored as recipient so can indicate that the
+			# stored recipient is the reciever
+			@reciever = @conversation.recipient
+		end
 		@messages = @conversation.messages
 		@message = Message.new
 	end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -10,6 +10,17 @@ class Conversation < ActiveRecord::Base
 		where("conversations.sender_id =? OR conversations.recipient_id=?", user.id, user.id)
 	end
 
+	def self.convo_exists?(user1_id, user2_id)
+		if Conversation.where(sender_id: user1_id, recipient_id: user2_id).any?
+		  return Conversation.where(sender_id: user1_id, recipient_id: user2_id).first.id
+		elsif Conversation.where(recipient_id: user1_id, sender_id: user2_id).any?
+			return Conversation.where(recipient_id: user1_id, sender_id: user2_id).first.id
+		else
+			return -1
+		end
+	end
+		
+
 	scope :between, -> (sender_id, recipient_id) do
 		where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id =? )", sender_id, recipient_id, recipient_id, sender_id)
 	end
