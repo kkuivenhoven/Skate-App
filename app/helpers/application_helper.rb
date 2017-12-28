1e83a6f2c5d63ece330a9bd33866c8cbaf25f1df
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2017-12-28 12:28:18 -0800

    Added method for retrieving skate spot user votes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	def UV_1(skate_spot)
+		total = skate_spot.user_votes.select{|key, hash| hash["1"] == "1" }.count
+		all = total.to_s + "&nbsp;"
+		return all.html_safe
+	end
+
+	def UV_0(skate_spot)
+		total = skate_spot.user_votes.select{|key, hash| hash["0"] == "0" }.count
+		all = total.to_s + "&nbsp;"
+		return all.html_safe
+	end
+
 end
