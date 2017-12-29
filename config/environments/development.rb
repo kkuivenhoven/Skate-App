786c4c5521ba60913279c59cf70dafcd142c9331
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2017-12-29 12:08:11 -0800

    caching js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,7 @@
+# Sources/tutorials/guides:
+# 	• https://technicalpickles.com/posts/some-tips-for-caching-javascript-and-css-in-rails
+#
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -11,7 +15,8 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
