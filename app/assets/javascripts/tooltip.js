9f95287112f4fb6de50141cf55ea9e8a083f7613
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-01-01 15:48:25 -0800

    added modal to prevent non-logged in users from seeing all material

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -1,7 +1,28 @@
 window.onload = function(){
-		//alert("hey");
+	var first = 0;
+	var pathname = window.location.pathname;	// returns path only
+	if((pathname == "/skate_spots") || (pathname == "/ratings/index") || ((pathname.search("/skate_spots")) == 0 ) || (pathname == "/events/index") || (pathname =="/static_pages/skate_links")){
+		$(window).scroll(function() {
+			var docSize = $(document).height();
+			var winSize = $(window).height();
+			var half_ds = docSize/4;
+			var winScrl = $(window).scrollTop();
+			if(first != 1){
+				if((winScrl+winSize) >= (3*half_ds)){
+						$('#preventModal').modal('show');
+						$(document.body).children('div').children('div').addClass("is-blurred");
+						$(document.body).children('div').children('div').last().removeClass("is-blurred");
+						first = 1;
+				}
+			}
+			$('#close_PM').click(function() {
+			  $(document.body).children('div').children('div').removeClass("is-blurred");
+					first = 0;
+			});
+		});
+	}
 }
 
 $(document).ready(function(){
-				    $('[data-toggle="tooltip"]').tooltip(); 
+			$('[data-toggle="tooltip"]').tooltip(); 
 });
