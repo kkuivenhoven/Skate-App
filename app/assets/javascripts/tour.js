53ffca8db5b06ffd38ac55095338f30c683665e5
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-01-01 17:44:44 -0800

    (Fix) Fixed for when user doesn't click the tour at all but navigates to different page. now tour still pops up opon new page loaded

--- app/assets/javascripts/tour.js
+++ app/assets/javascripts/tour.js
@@ -1,67 +1,58 @@
-//var restartTour = 0;
 $( document ).ready(function() {
 	var tour;
 	var user_logged_in = $('meta[name=logged_in_user]').attr("content");
 	if(user_logged_in == "true"){
-		tour = new Tour({
-			storage: false,
-			steps: [
-			{
-				element: $('#main_nav'),
-				title: "Main navigation links",
-				content: "This navigation bar contains regular links and dropdowns with more links. If there is an upside down triangle on the right side of the phrase, click the upside down triangle for more information!",
-				placement: "bottom"
-			},
-			{
-				element: $('#UO_nav'),
-				title: "User options",
-				content: "Links to your profile, settings, other users profiles, and the log out link!",
-				placement: "bottom"
-			},
-			{
-				element: $('#SS_nav'),
-				title: "Skate spot options",
-				content: "Links to all the skate spots! You can add your own skate spots through the new skate spot links - and can create based on your physical GPS location or by address!",
-				placement: "bottom"
-			},
-			{
-				element: $('#RO_nav'),
-				title: "Rating options",
-				content: "View all skate spots that have been rated along with their average ratings! Sorting option to available for finding toughest ratest spots with the least security and more!",
-				placement: "bottom"
-			},
-			{
-				element: $('#EO_nav'),
-				title: "Event options",
-				content: "View all events that are occuring in the future at documented skate spots!",
-				placement: "bottom"
-			},
-			{
-				element: $('#Res_nav'),
-				title: "Resources",
-				content: "Links to various skateboarding social media organizations!",
-				placement: "bottom"
-			}
-		]
-	});	
-		
-		tour.init();
+		if(tour == null){
+			tour = new Tour({
+				storage: false,
+				steps: [
+					{
+						element: $('#main_nav'),
+						title: "Main navigation links",
+						content: "This navigation bar contains regular links and dropdowns with more links. If there is an upside down triangle on the right side of the phrase, click the upside down triangle for more information!",
+						placement: "bottom"
+					},
+					{
+						element: $('#UO_nav'),
+						title: "User options",
+						content: "Links to your profile, settings, other users profiles, and the log out link!",
+						placement: "bottom"
+					},
+					{
+						element: $('#SS_nav'),
+						title: "Skate spot options",
+						content: "Links to all the skate spots! You can add your own skate spots through the new skate spot links - and can create based on your physical GPS location or by address!",
+						placement: "bottom"
+					},
+					{
+						element: $('#RO_nav'),
+						title: "Rating options",
+						content: "View all skate spots that have been rated along with their average ratings! Sorting option to available for finding toughest ratest spots with the least security and more!",
+						placement: "bottom"
+					},
+					{
+						element: $('#EO_nav'),
+						title: "Event options",
+						content: "View all events that are occuring in the future at documented skate spots!",
+						placement: "bottom"
+					},
+					{
+						element: $('#Res_nav'),
+						title: "Resources",
+						content: "Links to various skateboarding social media organizations!",
+						placement: "bottom"
+					}
+				]
+			});	
 
-		//if(restartTour == 0){
+			tour.init();
 			tour.start();
-		if(tour.ended()){
-			tour.restart();
 		}
-		/*}
-		if(restartTour == 1){*/
-			//tour.restart();
-		//}
-
+		else{
+			if(tour.ended()){
+				tour.restart();
+			}
+		}
 	}
-	/*if(user_logged_in == "false"){
-		restartTour = 1;
-		tour.end();
-	}*/
-		
 });
 
