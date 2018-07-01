0e9628db73d0e0841444d262b155819a0cef2d2a
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2018-07-01 02:20:54 -0700

    upon clicking a hi-lighted day, user is redirected to events index page

--- app/helpers/skate_spots_helper.rb
+++ app/helpers/skate_spots_helper.rb
@@ -3,7 +3,9 @@ module SkateSpotsHelper
 	def checkDate(newEvents, date, keyEvents)
 		if newEvents.include?(date.to_date)
 			@ev = Event.find_by(date: keyEvents[newEvents.index(date.to_date).to_i])
-			return "<span data-toggle='tooltip' data-placement='bottom' title='#{ @ev.name.capitalize } event @ #{ @ev.date.time.strftime("%l:%M %p") }' style='color: #f4ffe3'>#{ date.strftime("%e") }</span>".html_safe
+			return "<span class='skate_link' data-toggle='tooltip' data-placement='bottom' title='#{ @ev.name.capitalize } event @ #{ @ev.date.time.strftime("%l:%M %p") }' style='color: #f4ffe3'>
+							 #{ link_to date.strftime("%e"), events_path }
+					</span>".html_safe
 		else
 			return "#{ date.strftime("%e") }".html_safe
 		end
